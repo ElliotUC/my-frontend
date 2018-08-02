31f76672a1973132516d269c1fd0055d2b904375
ElliotUC <elliot.palominos@gmail.com>
2018-08-02 10:54:58 -0400

    Feature search component (#7)
    * The search component is added and stylized html
    
    * Fix color spinner

--- spotify-frontend/src/app/app.module.ts
+++ spotify-frontend/src/app/app.module.ts
@@ -2,18 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'; 
+import { AtomSpinnerModule } from 'angular-epic-spinners'
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './components/search/search.component';
 import { AlbumComponent } from './components/album/album.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    SearchComponent,
     AlbumComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    HttpClientModule,
+    FormsModule,
+    AtomSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
